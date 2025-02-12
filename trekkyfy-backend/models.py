d21be38d4bae5ceb30f21681f9834c1efb712e86
53Abdeali <aliabdealifakhri53@gmail.com>
2025-02-12 20:30:51 +0530

    Dashboard Backend Development Strated

--- trekkyfy-backend/models.py
+++ trekkyfy-backend/models.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), nullable=False)
+    username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(225), unique=True, nullable=False)
     role = db.Column(db.String(20), nullable=False, default="hiker")
