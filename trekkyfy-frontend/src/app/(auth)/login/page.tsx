2058e6364ad5a61343831fb4bef944cdf4789df9
53Abdeali <aliabdealifakhri53@gmail.com>
2025-02-12 16:34:59 +0530

    Dashboard Development Strated

--- trekkyfy-frontend/src/app/(auth)/login/page.tsx
+++ trekkyfy-frontend/src/app/(auth)/login/page.tsx
@@ -37,7 +37,7 @@ export default function Login() {
     }
 
     try {
-      const response = await axiosInstance.post("/api/login", {
+      const response = await axiosInstance.post("/login", {
         email,
         password,
       });
