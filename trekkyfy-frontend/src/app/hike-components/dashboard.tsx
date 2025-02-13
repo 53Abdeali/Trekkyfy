a97b0f1c29f1938a3bba0c55360124b9fd982bbd
53Abdeali <aliabdealifakhri53@gmail.com>
2025-02-13 18:04:08 +0530

    Home Page Development!

--- trekkyfy-frontend/src/app/hike-components/dashboard.tsx
+++ trekkyfy-frontend/src/app/hike-components/dashboard.tsx
@@ -2,21 +2,23 @@ import "@/app/stylesheet/dashboard.css";
 
 export default function Dashboard() {
   return (
-    <div>
-      <div className="main-dashboard">
-        <div className="dashboards">
-          <div className="dashboard">
-            <h3>Treks Completed</h3>
-            <p>0</p>
-          </div>
-          <div className="dashboard">
-            <h3>Distance Covered</h3>
-            <p>0 Km</p>
-          </div>
-          <div className="dashboard">
-            <h3>Upcoming Treks</h3>
-            <p>0</p>
-          </div>
+    <div className="main-dashboard">
+      <div className="dashboard-line"></div>
+      <div className="dashboard-head">
+        <h1>Your Dashboard</h1>
+      </div>
+      <div className="dashboards">
+        <div className="dashboard">
+          <h3>Treks Completed</h3>
+          <p>0</p>
+        </div>
+        <div className="dashboard">
+          <h3>Distance Covered</h3>
+          <p>0 Km</p>
+        </div>
+        <div className="dashboard">
+          <h3>Upcoming Treks</h3>
+          <p>0</p>
         </div>
       </div>
     </div>
