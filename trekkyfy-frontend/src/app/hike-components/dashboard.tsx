0311c777acee0944cca027c9a7878f735a67919d
53Abdeali <aliabdealifakhri53@gmail.com>
2025-02-13 17:21:15 +0530

    Home Page Development!

--- trekkyfy-frontend/src/app/hike-components/dashboard.tsx
+++ trekkyfy-frontend/src/app/hike-components/dashboard.tsx
@@ -1,5 +1,24 @@
-import "@/app/stylesheet/dashboard.css"
+import "@/app/stylesheet/dashboard.css";
 
-export default function Dashboard(){
-    return <div>I am a Dashboard</div>
+export default function Dashboard() {
+  return (
+    <div>
+      <div className="main-dashboard">
+        <div className="dashboards">
+          <div className="dashboard">
+            <h3>Treks Completed</h3>
+            <p>0</p>
+          </div>
+          <div className="dashboard">
+            <h3>Distance Covered</h3>
+            <p>0 Km</p>
+          </div>
+          <div className="dashboard">
+            <h3>Upcoming Treks</h3>
+            <p>0</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
