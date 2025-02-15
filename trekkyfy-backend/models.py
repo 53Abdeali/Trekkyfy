f54e3f463e8303e1825daa29ef0535931582f855
53Abdeali <aliabdealifakhri53@gmail.com>
2025-02-15 12:40:40 +0530

    APIs are separated

--- trekkyfy-backend/models.py
+++ trekkyfy-backend/models.py
@@ -8,7 +8,7 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(225), unique=True, nullable=False)
     role = db.Column(db.String(20), nullable=False, default="hiker")
-    guide_id = db.Column(db.String(10), nullable=True, unique=True)
+    guide_id = db.Column(db.String(10), index=True, nullable=True, unique=True)
     registered_on = db.Column(db.DateTime, default=datetime.utcnow())
 
     def __repr__(self):
