29e3bf6ec6c9457d7f5555cb3ce91743a90b91f0
53Abdeali <aliabdealifakhri53@gmail.com>
2025-02-16 13:40:34 +0530

    Guide Page Work Completed 50%

--- trekkyfy-backend/app.py
+++ trekkyfy-backend/app.py
@@ -97,6 +97,5 @@ def home():
 
 
 if __name__ == "__main__":
-    # port = int(os.environ.get("PORT", 10000))
-    # socketio.run(app, host="0.0.0.0", port=port)
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    socketio.run(app, host="0.0.0.0", port=port)
