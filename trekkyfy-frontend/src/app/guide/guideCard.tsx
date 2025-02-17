4f87bc5ce4a472a171df5e2956ebf43aed3ec738
53Abdeali <aliabdealifakhri53@gmail.com>
2025-02-17 12:02:39 +0530

    Guide Page is 90% Ready

--- trekkyfy-frontend/src/app/guide/guideCard.tsx
+++ trekkyfy-frontend/src/app/guide/guideCard.tsx
@@ -1,8 +1,7 @@
 "use client";
 
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
-import { io, Socket } from "socket.io-client";
 import GuideModal from "./GuideModal";
 import "@/app/stylesheet/guideCard.css";
 
