43208e4c584e4bd32842cf1202f1e1121b1a0eef
Abdeali@53 <aliabdealifakhri53@gmail.com>
2025-02-24 12:07:00 +0530

    Guide Page 99% done

--- trekkyfy-backend/routes/hiker_info.py
+++ trekkyfy-backend/routes/hiker_info.py
@@ -26,11 +26,11 @@ def price_avl_req():
     trek_place = data.get("trek_place")
     hiking_members = data.get("hiking_members")
     trek_date = datetime.strptime(data.get("trek_date"), "%Y-%m-%d").date()
-    trek_time_str = datetime.strptime(data.get("trek_time"), "%H:%M:%S").time()
-    
-    if len(trek_time_str) == 5:  
+    trek_time_str = data["trek_time"]
+
+    if len(trek_time_str) == 5:
         trek_time = datetime.strptime(trek_time_str, "%H:%M").time()
-    elif len(trek_time_str) == 8: 
+    elif len(trek_time_str) == 8:
         trek_time = datetime.strptime(trek_time_str, "%H:%M:%S").time()
     else:
         return (
